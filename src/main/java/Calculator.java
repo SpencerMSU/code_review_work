e2ab8e0f6a425e0ccf9b81d1c3f51f1197b15e87
Егор <165308109+SpencerMSU@users.noreply.github.com>
2026-05-15 16:03:37 +0300

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,71 +2,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
-    public int add(int a, int b){
 
-        //TODO inser your realisation in method add
+    public int add(int a, int b) {
         return a + b;
     }
-    public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+
+    public int dif(int a, int b) {
         return a - b;
     }
-    public int div(int a, int b){
-        //TODO inser your realisation in method div
+
+    public int div(int a, int b) {
         if (b == 0) {
-            throw new ArithmeticException("Error 0");
+            throw new ArithmeticException("Деление на ноль!");
         }
         return a / b;
     }
-    public int times(int a, int b){
-        //TODO inser your realisation in method times
+
+    public int times(int a, int b) {
         return a * b;
     }
-    public List<Integer> solver(int a, int b, int c){
+
+    public List<Integer> solver(int a, int b, int c) {
         List<Integer> roots = new ArrayList<>();
-        int discriminant = b*b - 4*a*c;
+        int discriminant = b * b - 4 * a * c;
 
         if (discriminant < 0) {
             roots.add(-1);
         } else if (discriminant == 0) {
-            roots.add(-b / (2*a));
+            roots.add(-b / (2 * a));
         } else {
-            roots.add((int)((-b + Math.sqrt(discriminant)) / (2*a)));
-            roots.add((int)((-b - Math.sqrt(discriminant)) / (2*a)));
+            roots.add((int) ((-b + Math.sqrt(discriminant)) / (2 * a)));
+            roots.add((int) ((-b - Math.sqrt(discriminant)) / (2 * a)));
         }
         return roots;
-
-        return a + b;
-    }
-    public int dif(int a, int b){
-        return a - b;
-    }
-    public int div(int a, int b){
-
-        if (b == 0) {
-            throw new ArithmeticException("Деление на ноль!");
-        }
-
-        if (b == 0) return 0; // Базовая проверка на ноль
-
-        return a / b;
-    }
-    public int times(int a, int b){
-        return a * b;
     }
 
-    public int solver(){
-
-        int step1 = add(5, 3);
+    public int solver() {
+        int step1 = add(10, 5);
         int step2 = times(step1, 2);
-        int step3 = div(4, 2);
-        int result = dif(step2, step3);
-        return result;
-
-        int step1 = add(10, 5);      // 15
-        int step2 = times(step1, 2);  // 30
-        int step3 = div(20, 4);      // 5
-        return dif(step2, step3);    // 25
-
+        int step3 = div(20, 4);
+        return dif(step2, step3);
     }
 }
